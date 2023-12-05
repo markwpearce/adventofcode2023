0807c741f92fed53b6a856410f91da43ef467a4a
Mark Pearce <mark.pearce@redspace.com>
2023-12-05 11:15:57 -0400

    solved day2-part 1

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,9 +1,22 @@
 use crate::util::read_data;
 
 const TEST_DATA: [&str; 4] = ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"];
+const TEST_DATA2: [&str; 7] = [
+    "two1nine",
+    "eightwothree",
+    "abcone2threexyz",
+    "xtwone3four",
+    "4nineeightseven2",
+    "zoneight234",
+    "7pqrstsixteen",
+];
+
+const NUMBER_STRS: [&str; 10] = [
+    "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+];
 
 pub fn part1() {
-    let mut sum = 0;
+    let mut sum: i32 = 0;
 
     let data = read_data::read_lines("./data/day01.txt");
 
@@ -14,20 +27,113 @@ pub fn part1() {
     println!("{sum}");
 }
 
-fn first_int(my_str: &str) -> u32 {
+fn first_int(my_str: &str) -> i32 {
     for c in my_str.chars() {
         if c.is_numeric() {
-            return c.to_digit(10).unwrap();
+            return c.to_digit(10).unwrap() as i32;
         }
     }
-    return 11;
+    return -1;
 }
 
-fn last_int(my_str: &str) -> u32 {
+fn last_int(my_str: &str) -> i32 {
     for c in my_str.chars().rev() {
         if c.is_numeric() {
-            return c.to_digit(10).unwrap();
+            return c.to_digit(10).unwrap() as i32;
         }
     }
-    return 11;
+    return -1;
+}
+
+pub fn part2() {
+    let mut sum = 0;
+
+    let data = read_data::read_lines("./data/day01.txt");
+
+    for test_str in data {
+        let first = first_int_str(&*test_str);
+        let last = last_int_str(&*test_str);
+        let word_val = first * 10 + last;
+        println!("{test_str}: {first} {last} {word_val}");
+
+        sum = sum + word_val;
+    }
+    println!("{sum}");
+}
+
+fn check_string_for_string(
+    my_str: &str,
+    search_str: &str,
+    in_val: i32,
+    first_index: &mut u32,
+    value: &mut i32,
+) -> bool {
+    let found = my_str.find(search_str);
+    if found.is_none() {
+        return false;
+    }
+    let found_index = found.unwrap() as u32;
+    if found_index < *first_index {
+        first_index.clone_from(&found_index);
+        value.clone_from(&in_val);
+        return true;
+    }
+    return false;
+}
+
+fn check_string_for_last_string(
+    my_str: &str,
+    search_str: &str,
+    in_val: i32,
+    last_index: &mut u32,
+    value: &mut i32,
+) -> bool {
+    let found = my_str.rfind(search_str);
+    if found.is_none() {
+        return false;
+    }
+    let found_index = found.unwrap() as u32;
+    if found_index > *last_index {
+        last_index.clone_from(&found_index);
+        value.clone_from(&in_val);
+        return true;
+    }
+    return false;
+}
+
+fn first_int_str(my_str: &str) -> i32 {
+    let mut first = my_str.len() as u32;
+    let mut value = -1;
+
+    for i in 0..10 {
+        check_string_for_string(my_str, NUMBER_STRS[i], i as i32, &mut first, &mut value);
+    }
+    let mut i = 0;
+    for c in my_str.chars() {
+        if c.is_numeric() && i < first {
+            return c.to_digit(10).unwrap() as i32;
+        }
+        if i > first {
+            break;
+        }
+        i += 1;
+    }
+    return value;
+}
+
+fn last_int_str(my_str: &str) -> i32 {
+    let mut last = 0;
+    let mut value = -1;
+
+    for i in 0..10 {
+        check_string_for_last_string(my_str, NUMBER_STRS[i], i as i32, &mut last, &mut value);
+    }
+
+    for i in last..(my_str.len() as u32) {
+        let c = my_str.chars().nth(i as usize).unwrap();
+        if c.is_numeric() {
+            value = c.to_digit(10).unwrap() as i32;
+        }
+    }
+    return value;
 }
---
+++ src/days/day02.rs
@@ -0,0 +1,103 @@
+use crate::util::read_data;
+use regex::Regex;
+use std::fmt;
+
+const TEST_DATA: [&str; 5] = [
+    "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+    "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+    "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+    "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+    "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+];
+
+struct Round {
+    red: i32,
+    green: i32,
+    blue: i32,
+}
+
+struct Game {
+    id: i32,
+    rounds: Vec<Round>,
+}
+
+impl fmt::Display for Game {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut rounds_str = "".to_owned();
+        for round in &self.rounds {
+            if rounds_str.len() > 0 {
+                rounds_str.push_str(", ");
+            }
+            rounds_str.push_str(
+                format!("(R: {}, G: {}, B: {})", round.red, round.green, round.blue).as_str(),
+            );
+        }
+        write!(f, "{}, [{}]", self.id, rounds_str)
+    }
+}
+
+fn parse_game(game_str: &str) -> Game {
+    let re = Regex::new(r"Game (\d+)\: (.*)").unwrap();
+    //let id_match = re.captures(game_str).try_into();
+    let mut game_id = "";
+    let mut rounds_str = "";
+    for (_, [id, rounds]) in re.captures_iter(game_str).map(|c| c.extract()) {
+        game_id = id;
+        rounds_str = rounds;
+        break;
+    }
+    let mut rounds_vec: Vec<Round> = Vec::new();
+    let round_red = Regex::new(r"(\d+) red").unwrap();
+    let round_green = Regex::new(r"(\d+) green").unwrap();
+    let round_blue = Regex::new(r"(\d+) blue").unwrap();
+    for round in rounds_str.split(";") {
+        let red_str = match round_red.captures(round) {
+            Some(value) => value.get(1).unwrap().as_str(),
+            None => "0",
+        };
+        let green_str = match round_green.captures(round) {
+            Some(value) => value.get(1).unwrap().as_str(),
+            None => "0",
+        };
+        let blue_str = match round_blue.captures(round) {
+            Some(value) => value.get(1).unwrap().as_str(),
+            None => "0",
+        };
+
+        rounds_vec.push(Round {
+            red: red_str.parse::<i32>().unwrap(),
+            green: green_str.parse::<i32>().unwrap(),
+            blue: blue_str.parse::<i32>().unwrap(),
+        });
+    }
+
+    let game = Game {
+        id: game_id.to_string().parse().unwrap(), //id_match.unwrap().as_str().parse().unwrap(),
+        rounds: rounds_vec,
+    };
+    return game;
+}
+
+pub fn part1() {
+    let data = read_data::read_lines("./data/day02.txt");
+    let mut sum = 0;
+    for test_str in data {
+        let game = parse_game(&test_str);
+        if check_game(&game) {
+            sum += game.id.abs();
+        }
+    }
+    println!("{sum}");
+}
+
+fn check_game(g: &Game) -> bool {
+    let max_red = 12;
+    let max_green = 13;
+    let max_blue = 14;
+    for r in &g.rounds {
+        if r.red > max_red || r.green > max_green || r.blue > max_blue {
+            return false;
+        }
+    }
+    return true;
+}
